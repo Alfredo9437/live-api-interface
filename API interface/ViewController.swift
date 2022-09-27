af65f04a602777c0efcd152df80cc1242f2aa923
Alfredo Rodriguez Garcia <alfredor94379@csu.fullerton.edu>
2022-09-27 16:12:16 -0700

    Initial Commit

---
+++ API interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API interface
+//
+//  Created by Alfredo Rodriguez-Garcia on 9/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
