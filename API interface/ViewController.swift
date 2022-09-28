3674b699b202ad921add8fd0e96f6835b55ea8e9
Alfredo Rodriguez Garcia <alfredor94379@csu.fullerton.edu>
2022-09-27 17:33:13 -0700

    Pushing the classwork

--- API interface/ViewController.swift
+++ API interface/ViewController.swift
@@ -8,12 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var firstButton: UIButton!
+    var secondButton: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func loadView() {
+        //have to loadview first
+        super.loadView()
+        //make first button
+        self.firstButton = UIButton(type: .system)
+        self.firstButton.translatesAutoresizingMaskIntoConstraints = false
+        self.firstButton.setTitle(
+            "First Button",
+            for: UIControl.State.normal
+        )
+        self.firstButton.addTarget(self,
+                                   action: #selector(firstButtonClicked(_:)),
+                                                     for: UIControl.Event.touchUpInside)
+        self.firstButton.addTarget(self,
+                                   action: #selector(firstButtonDragExited(_:)),
+                                                     for: UIControl.Event.touchDragExit)
+        
+        
+        //make second buttton
+        self.secondButton = UIButton(type: UIButton.ButtonType.system)
+        self.secondButton.translatesAutoresizingMaskIntoConstraints = false
+        self.secondButton.setTitle(
+            "Second Button",
+            for: UIControl.State.normal
+        )
+        self.secondButton.frame = CGRect(
+            x:10, y:200,
+            width: 150, height: 100
+        
+        )
+        self.secondButton.addTarget(self,
+                                   action: #selector(secondButtonClicked(_:)),
+                                                     for: UIControl.Event.touchUpInside)
+        //add the buttons to our prim view
+        self.view.addSubview(self.firstButton)
+        self.view.addSubview(self.secondButton)
+        
+        //create the constraint and store to a variable
+        //so we can activate later
+        //must happen after weve added the buttons to the view
+        let b1CenterX = self.firstButton.centerXAnchor.constraint(
+            equalTo: self.view.safeAreaLayoutGuide.centerXAnchor)
+        let b1CenterY = self.firstButton.centerYAnchor.constraint(
+            equalTo: self.view.safeAreaLayoutGuide.centerYAnchor)
+        b1CenterX.isActive = true
+        b1CenterY.isActive = true
+        
+
+    }
 
 
+    
+
+    @objc func firstButtonClicked(_ b: UIButton){
+        print("First Button Clicked")
+    }
+    @objc func firstButtonDragExited(_ b: UIButton){
+        print("First button dragged")
+    }
+
+    
+    @objc func secondButtonClicked(_ b: UIButton){
+        print("Second Button Clicked")
+    }
+    
 }
 
